0ccf828c84a7d60dd02ce294e29b31cef6673916
Hermann Pereira <hermannhegelpereira@gmail.com>
2024-12-28 19:16:25 -0300

    adiciona dependência de dev `Prettier`

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # clone.tabnews
+
 Treinando o básico (denovo) <br>
 ~~Dia 2~~ (concluído) <br>
 ~~Dia 3~~ (concluído) <br>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Se vc é irmã do Ryan, dê uma risadinha.</h1>;
+  return <h1>Se vc é irmã do Ryan, dê uma risadinha.</h1>;
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default Home;
